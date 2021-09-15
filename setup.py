70c0bc695627ee508170a9f6c6ed54ce25c9a62c
tiago1figueira <tiago_figueira@hotmail.com>
2021-09-15 16:02:40 -0300

    Criação do módulo setup.py.

--- setup.py
+++ setup.py
@@ -63,10 +63,9 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print(sys.stderr, (
+                            print >> sys.stderr, (
                                     "Directory %s ignored by pattern %s"
                                     % (fn, pattern))
-                                  )
                         break
                 if bad_name:
                     continue
@@ -87,10 +86,9 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print(sys.stderr, (
+                            print >> sys.stderr, (
                                     "File %s ignored by pattern %s"
                                     % (fn, pattern))
-                                  )
                         break
                 if bad_name:
                     continue
