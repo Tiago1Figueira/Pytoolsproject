b5bae1f481fb2b9fb743ebf16a17f7374073924f
tiago1figueira <tiago_figueira@hotmail.com>
2021-10-20 13:17:51 -0300

    Criaçao módulo teste_usuario.py.

---
+++ Pytoolsproject/tests/test_spam/test_usuarios.py
@@ -0,0 +1,34 @@
+import pytest
+
+from Pytoolsproject.spam.db import Conexao
+from Pytoolsproject.spam.modelos import Usuario
+
+
+@pytest.fixture
+def conexao():
+    # Execução da fase de setup das fixtures.
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # Execução da fase de tear down das fixtures.
+    conexao_obj.fechar() #obj = objeto
+
+
+@pytest.fixture
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao() #sessao_obj = objeto
+    yield sessao_obj
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
+    usuario= Usuario(nome='Renzo')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuarios(sessao):
+    usuarios= [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
