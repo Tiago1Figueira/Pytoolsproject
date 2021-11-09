dbf2dbc7d05482bd6fd321372061157299293eb8
moderno <tiago_figueira@hotmail.com>
2021-11-08 22:26:57 -0300

    Transferências das fixtures deste módulo para o conftest.

--- Pytoolsproject/tests/test_spam/test_usuarios.py
+++ Pytoolsproject/tests/test_spam/test_usuarios.py
@@ -1,24 +1,4 @@
-import pytest
-
-from Pytoolsproject.spam.db import Conexao
 from Pytoolsproject.spam.modelos import Usuario
-
-
-@pytest.fixture
-def conexao():
-    # Execução da fase de setup das fixtures.
-    conexao_obj = Conexao()
-    yield conexao_obj
-    # Execução da fase de tear down das fixtures.
-    conexao_obj.fechar()   # obj = objeto
-
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()   # sessao_obj = objeto
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuario(sessao):
