edb3d59f520ab39e8367b060a9102831e4a66e96
moderno <tiago_figueira@hotmail.com>
2021-11-17 22:42:55 -0300

    Reorganização do código e criação da função test_qde_de_spam().

--- Pytoolsproject/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ Pytoolsproject/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -9,13 +9,37 @@ class EnviadorMock(Enviador):
 
     def __init__(self):
         super().__init__()
-        self.qtd_emails_enviados = 0
+        self.qtd_email_enviados = 0
         self.parametros_de_envio = None
 
     def enviar(self, remetente, destinatario, assunto, corpo):
         self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_emails_enviados += 1
+        self.qtd_email_enviados += 1
 
+
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Renzo', email='renzo@python.pro.br'),
+            Usuario(nome='Luciano', email='renzo@python.pro.br')
+        ],
+        [
+            Usuario(nome='Renzo', email='renzo@python.pro.br'),
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'renzo@python.pro.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert len(usuarios) == enviador.qtd_email_enviados
 
 
 def test_parametros_de_spam(sessao):
@@ -34,5 +58,3 @@ def test_parametros_de_spam(sessao):
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
-
-
